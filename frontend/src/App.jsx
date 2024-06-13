913b52c86f6a95618e6d8d8ce39a8db45ffb367f
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-06-13 18:11:21 +0200

    Some changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LoginForm from "./components/login";
+import RegisterForm from "./components/register";
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/register">
+            <RegisterForm />
+          </Route>
+          <Route path="/">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
